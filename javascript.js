6e381cc0f57fa22a1311e8d4bc520273be7a368c
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-06-01 12:25:22 -0700

    Added tab display and hiding on click.

--- javascript.js
+++ javascript.js
@@ -4,6 +4,12 @@ let about = document.getElementById('resume');
 let apps = document.getElementById('apps');
 let contact = document.getElementById('contact');
 
+let homePage = document.getElementById('home_page');
+let resumePage = document.getElementById('about_page');
+let aboutPage = document.getElementById('resume_page');
+let appsPage = document.getElementById('apps_page');
+let contactPage = document.getElementById('contact_page');
+
 home.addEventListener("click", reveal);
 resume.addEventListener("click", reveal);
 about.addEventListener("click", reveal);
@@ -16,13 +22,33 @@ function reveal(e) {
   showTab(targetChoice)
 }
 
-function showTab() {
-  switch (expression) {
+function showTab(choice) {
+  switch (choice) {
+    case "home":
+      console.log(homePage)
+      homePage.style.display = "block"
+      break
+    case "about":
+      aboutPage.style.display = "block"
+      break;
+    case "resume":
+      resumePage.style.display = "block"
+      break;
+    case "apps":
+      appsPage.style.display = "block"
+      break;
+    case "contact":
+      contactPage.style.display = "block"
+      break;
   }
 }
 
-function hideTab() {
-
+function hideTab(choice) {
+      homePage.style.display = "none"
+      aboutPage.style.display = "none"
+      resumePage.style.display = "none"
+      appsPage.style.display = "none"
+      contactPage.style.display = "none"
 }
 function createBubble() {
     const section = 
